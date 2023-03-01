eeb55545372e766feb95355a22d297cbb96f61b1
Michelle <michellehoff97@gmail.com>
2023-03-01 21:23:05 +0100

    lectures table, binding

--- webapp/controller/LecturesList.controller.js
+++ webapp/controller/LecturesList.controller.js
@@ -1,12 +1,18 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel", 'sap/m/library'],
+  function (Controller, JSONModel, library) {
     "use strict";
 
-    return Controller.extend("sap.ui.demo.webtech,controller.InvoiceList", {
-        onInit: function () {
-           
-        }
+    return Controller.extend("sap.ui.demo.webtech.controller.LecturesList", {
+      onInit: function () {
+        var oModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/webtech/Lectures.json"));
+			this.getView().setModel(oModel);
+      },
+      formatDate: function(sDate) {
+        var oDate = new Date(sDate);
+        const options= {weekday:'long', year:'numeric', month:'long', day:'numeric'};
+        return oDate.toLocaleDateString('de-DE',options);
+      }
     });
-});
+  }
+);
